d348be9f27f9cf92649b96200b7946f00dcbd10e
sarahbenny <47547719+sarahbenny@users.noreply.github.com>
2019-03-07 10:27:00 -0800

    Add files via upload

--- untitled3.py
+++ untitled3.py
@@ -1,43 +1,27 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jan 28 16:58:48 2019
+Created on Mon Jan 28 16:58:48 201
 
 @author: sarahbenny
 """
-
-filename= 'new3.txt'
-infile= open(filename, 'r')
-line= infile.readline()
+filename= open( 'file2.txt', 'r')
 Zn_list=[]
 Cl_list=[]
 O_list=[]
 H_list=[]
-for line in infile:
-    s=line.split()
-    for Zn in s:
-        Zn_list.append(line)
-        print Zn_list
-for line in infile:
-    l=line.split()
-    for Cl in l:
-        Cl_list.append(line)
-        print Cl_list
-for line in infile:
-    a=line.split()
-    for O in a:
-        O_list.append(line)
-        print O_list
-for line in infile:
-    u=line.split()
-    for H in u:
-        H_list.append(line)
-        print H_list
 
-    
-   
-    
-    
-    
-       
-   
+for line in filename:
+    s=line.split()
+    if s[0] == 'Zn':
+        Zn_list.append(line)
+        print(Zn_list)
+        if s[0] == 'Cl':
+             Cl_list.append(line)
+             print(Cl_list)
+        if s[0] == 'O':
+             O_list.append(line)
+             print(O_list)
+        if s[0] == 'H':
+            H_list.append(line)
+            print(H_list)  
