c7e72c5bf6dfca7c0d280cfa1dd99006c67248e4
sally008 <sarah.allec@gmail.com>
2019-03-15 11:52:08 -0700

    Update to write distances for different pairs of elements to separate files

--- pdf/water-example/get_distances.py
+++ pdf/water-example/get_distances.py
@@ -18,15 +18,18 @@ for line in el:
 md_car=open(md_car_file)
 lines=md_car.readlines()
 n_steps=len(lines)/(n_atoms+7)
-for i in range(n_steps):
-    a=lines[2+(n_atoms+7)*i]
+hh_file=open('hh_distance.txt','w')
+oh_file=open('oh_distance.txt','w')
+
+for n in range(n_steps):
+    a=lines[2+(n_atoms+7)*n]
     ax,ay,az=a.split()
     ax=float(ax)
     ay=float(ay)
     az=float(az)
     a_mag=math.sqrt(ax*ax+ay*ay+az*az)
 
-    b=lines[3+(n_atoms+7)*i]
+    b=lines[3+(n_atoms+7)*n]
     bx,by,bz=b.split()
     bx=float(bx)
     by=float(by)
@@ -34,7 +37,7 @@ for i in range(n_steps):
     b_mag=math.sqrt(bx*bx+by*by+bz*bz)
 
 
-    c=lines[4+(n_atoms+7)*i]
+    c=lines[4+(n_atoms+7)*n]
     cx,cy,cz=c.split()
     cx=float(cx)
     cy=float(cy)
@@ -45,7 +48,7 @@ for i in range(n_steps):
     oh=[]
     hh=[]
     for j in range(7,(n_atoms+7)):
-        x, y, z=lines[j+(n_atoms+7)*i].split()
+        x, y, z=lines[j+(n_atoms+7)*n].split()
         coords.append([a_mag*float(x), b_mag*float(y), c_mag*float(z)])
     atoms=Atoms(elements,positions=coords,cell=[[ax,ay,az],[bx,by,bz],[cx,cy,cz]])
     distances=np.asarray(atoms.get_all_distances(mic=True))
@@ -55,5 +58,16 @@ for i in range(n_steps):
                 oh.append(distances[i][j])
             if ( (elements[i] == 'H' and elements[j] == 'H') or (elements[i] == 'H' and elements[j] == 'H') ):
                 hh.append(distances[i][j])
+    #Write to files
+    #HH
+    hh_file.write((str(n+1)))
+    for h in hh:
+        hh_file.write('\t'+str(h))
+    hh_file.write('\n')
+    #OH
+    oh_file.write((str(n+1)))
+    for o in oh:
+        oh_file.write('\t'+str(o))
+    oh_file.write('\n')
 
 md_car.close
